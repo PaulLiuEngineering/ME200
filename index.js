d7b4a0f5e4a19543415e7ea47fd5a6cd2225232d
Paul Liu <paulliu@wirelessprv-10-193-22-2.near.illinois.edu>
2018-09-27 16:15:00 -0500

    sec

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var n = 0;
 $(document).ready(function() {
     $("area").mouseover(function (e) {
-        $(this).effect('highlight','swing','3000');
+        var str = $(this).attr('coords').toString();
+        console.log(str);
+        // $(this).effect('highlight','swing','3000');
     });
 });
