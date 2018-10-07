40dfb25e3bc7f7f01f985b67a1980d8db5d8b8b7
PaulLiuEngineering <paulliu14@gmail.com>
2018-10-07 11:19:30 -0500

    'lklk'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var n = 0;
 $(document).ready(function() {
-    $("area").mouseover(function (e) {
-        var str = $(this).attr('coords').toString();
-        console.log(str);
-        // $(this).effect('highlight','swing','3000');
-    });
+        // var str = $(this).attr('id');
+        // console.log(str);
+        $('.mapp').maphilight();
+        
+        //console.log(($(this).attr('id') || null));
+        
+        // $(this).effect('highlight','swing','3000')
 });
